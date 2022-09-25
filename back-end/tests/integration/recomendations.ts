56abe0e9ecc7a528ea10e65980f3c2f2d376fb35
Tiago <tigolimab223@gmail.com>
2022-09-25 18:03:13 -0300

    Upvote and Downvote integration test added

--- back-end/tests/integration/recomendations.ts
+++ back-end/tests/integration/recomendations.ts
@@ -1,6 +1,8 @@
 import supertest from "supertest";
 import app from "../../src/app";
 import { prisma } from "../../src/database";
+import { recommendationRepository } from "../../src/repositories/recommendationRepository";
+import { idFactory } from "../factories/idFactory";
 import { recommendationFactory } from "../factories/recommendationFactory";
 
 export async function recommendationTest() {
@@ -18,12 +20,46 @@ export async function recommendationTest() {
       expect(createdRecommendation).not.toBeNull();
     });
     it("Should try to create a recommendation that already exist", async () => {
-        const recommendation = recommendationFactory();
+      const recommendation = recommendationFactory();
 
-        await server.post("/recommendations").send(recommendation);
-        const result = await server.post("/recommendations").send(recommendation);
+      await server.post("/recommendations").send(recommendation);
+      const result = await server.post("/recommendations").send(recommendation);
 
-        expect(result.status).toBe(409);
-      });
+      expect(result.status).toBe(409);
+    });
+    it("Should give an upvote", async () => {
+      const recommendation = recommendationFactory();
+
+      await server.post("/recommendations").send(recommendation);
+      const { id } = await recommendationRepository.findByName(
+        recommendation.name
+      );
+
+      const result = await server.post(`/recommendations/${id}/upvote`);
+      expect(result.status).toBe(200);
+    });
+    it("Should not give an upvote and it should return 404", async () => {
+      const id = idFactory(3);
+      const result = await server.post(`/recommendations/${id}/upvote`);
+      expect(result.status).toBe(404);
+    });
+    it("Should give a downvote", async () => {
+      const recommendation = recommendationFactory();
+
+      await server.post("/recommendations").send(recommendation);
+
+      const { id } = await recommendationRepository.findByName(
+        recommendation.name
+      );
+
+      const result = await server.post(`/recommendations/${id}/downvote`);
+      expect(result.status).toBe(200);
+    });
+    it("Should not give a downvote and it should return 404", async () => {
+      const id = idFactory(3);
+
+      const result = await server.post(`/recommendations/${id}/downvote`);
+      expect(result.status).toBe(404);
+    });
   });
 }
